b3727b6f0aea2dd7d8aee737115398ed53e38e17
Biccira <makstur05@bk.ru>
2024-08-10 17:31:44 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+Home_work = 12
+Hours_spent = 1.5
+Course_name = 'Python'
+exemple = (Hours_spent) / (Home_work)
+print('Курс:',Course_name,'всего задач:',Home_work,'затрачено часов',Hours_spent,'среднее время выполнения',exemple)
